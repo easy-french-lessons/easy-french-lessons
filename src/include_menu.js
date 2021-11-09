518332aa5af26b4c5e8c4a3bc7fe3b8d9ffcc728
kcacooler <kcacooler@gmail.com>
2021-11-09 01:08:41 +0100

    Added verb lesson

---
+++ src/include_menu.js
@@ -0,0 +1,26 @@
+/*
+document.write("
+<div class="navbar">
+	<a href="index.html">Home</a>
+	<div class="dropdown">
+		<button class="dropbtn">Beginner
+			<i class="fa fa-caret-down"></i>
+		</button>
+		<div class="dropdown-content">
+			<a href="#">Basic sentences</a>
+		</div>
+	</div>
+</div>
+")
+*/
+
+var menu = document.getElementById("menu_bar");
+main_div = document.createElement("div");
+main_div.class = "navbar";
+
+var homeLink = createElement("a");
+homeLink.href = "index.html";
+homeLink.innerHTML = "Home";
+
+main_div.appendChild()
+
